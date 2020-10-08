880915acfd341066ba79d7231242ca2c7eca0ef5
Marcus Zillner <60900721+MarcusMadMax@users.noreply.github.com>
2020-10-09 12:55:03 +1300

    first commit

---
+++ wp-content/themes/weebiz/index.php
@@ -0,0 +1,15 @@
+<?php get_header()?>
+    <!-- Features -->
+    <div class="container index">
+        <?php 
+        
+            while ( have_posts() ) {
+                the_post(); 
+                get_template_part('partials/content','index');
+            } // end while
+
+        ?>
+    </div>
+<?php get_footer()?>
+
+   
